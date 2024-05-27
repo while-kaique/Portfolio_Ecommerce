671c9aeac2380a131562252db5e9c546e6e38d81
KaiCode <kaiquemedeiros440@gmail.com>
2024-05-27 16:55:00 -0300

    Criado Navbar
    Adicionei uma fonte ao projeto via html <link>.
    
    Falta apenas estilizar e definir as opções

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,56 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-gray-400 w-full h-1/5 fle flex-wrap">
+      <div className="w-full h-3/4">Informações do Jorge</div>
+      <nav>
+        <ul className="w-full flex justify-center gap-14 uppercase font-bold text-sm font-['Mulish'] relative">
+          <li className="relative ul_li "><h1 className="pb-2 hover:cursor-pointer">Mais Desejados</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul>
+          </li>
+          <li className="relative ul_li"><h1 className="pb-2 hover:cursor-pointer">bebês</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul></li>
+          <li className="relative ul_li"><h1 className="pb-2 hover:cursor-pointer">menina</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul></li>
+          <li className="relative ul_li"><h1 className="pb-2 hover:cursor-pointer">menino</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul></li>
+          <li className="relative ul_li"><h1 className="pb-2 hover:cursor-pointer">pijamas</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul></li>
+          <li className="relative ul_li"><h1 className="pb-2 hover:cursor-pointer">bazar</h1>
+            <ul className="submenu_maisDesejados">
+              <li><a href="#">opção 1</a></li>
+              <li><a href="#">opção 2</a></li>
+              <li><a href="#">opção 3</a></li>
+              <li><a href="#">opção 4</a></li>
+            </ul></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
