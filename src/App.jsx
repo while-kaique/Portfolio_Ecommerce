2f9e22ad7fa468e23930bac263292aec09c88334
KaiCode <kaiquemedeiros440@gmail.com>
2024-05-27 15:11:43 -0300

    Change Name of Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom'
 
-import Navbar from './components/Navbar.jsx'
+import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Header/>
         <Outlet/>
       <Footer/>
     </>
