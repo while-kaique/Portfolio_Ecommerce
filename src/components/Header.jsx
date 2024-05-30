14f14e1df293734a7ac177f616fbb4e05dc53753
KaiCode <kaiquemedeiros440@gmail.com>
2024-05-29 21:40:35 -0300

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,33 +4,33 @@ import { PiShoppingCartSimpleBold } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <header className="bg-white w-full h-1/5 shadow-navbar z-10">
+    <header className=" w-full h-1/5 shadow-navbar z-10">
       <div className=" flex justify-around items-center w-full h-3/4">
         <a href="#" className="w-24 h-12 bg-gray-400">logo</a>
         <div className="searcher relative">
           <input
             type="text"
-            className="bg-gray-100 w-96 rounded-lg focus:outline-none p-2 pl-5 text-gray-500"
+            className="bg-gray-200 w-[450px] rounded-lg focus:outline-none p-2 pl-5 text-gray-500"
             placeholder="Pesquisar ">
            </input>
-          <FiSearch className="absolute right-1 top-1/2 p-2 text-3xl bg-gray-800 text-white rounded-lg -translate-y-1/2 cursor-pointer" ></FiSearch >
+          <FiSearch className="absolute right-1 top-1/2 p-1.5 text-3xl bg-gray-800 text-white rounded-lg -translate-y-1/2 cursor-pointer" ></FiSearch >
         </div>
 
-        <div className="icons   flex  justify-around gap-5 ">
-          <div className="user relative   w-32  flex gap-3">
-             <PiUserBold className="absolute user -left-7 top-1/2 p-0.5 text-2xl rounded-lg -translate-y-1/2  cursor-pointer" ></PiUserBold >
-            <p>Login</p>
+        <div className="icons  flex justify-around gap-5 ">
+          <div className="user relative cursor-pointer flex gap-3">
+             <PiUserBold className="absolute user -left-7 top-1/2 p-0.5 text-2xl text-index-blue rounded-lg -translate-y-1/2  cursor-pointer" ></PiUserBold >
+            <p className="font-medium text-base">Entre ou Cadastre-se</p>
           </div>
 
-          <div className="cart relative   w-32  flex gap-3">
-             < PiShoppingCartSimpleBold className="absolute cart -left-7 top-1/2 p-0.5 text-2xl rounded-lg -translate-y-1/2  cursor-pointer" ></ PiShoppingCartSimpleBold >
-            <p>Carrinho</p>
+          <div className="cart relative ml-16 cursor-pointer flex gap-3">
+             < PiShoppingCartSimpleBold className="absolute cart -left-7 top-1/2 p-0.5 text-2xl text-index-blue rounded-lg -translate-y-1/2  cursor-pointer" ></ PiShoppingCartSimpleBold >
+            <p className="font-medium text-base">Meu Carrinho</p>
           </div>
         </div>
         <div></div>
       </div>
       <nav className="h-1/4 flex items-end">
-        <ul className="w-3/5 mx-auto flex justify-center uppercase font-semibold text-sm font-['Mulish'] relative">
+        <ul className="w-3/5 mx-auto flex justify-center uppercase font-bold font-['Mulish'] relative">
           <li className="relative ul_li">
             <h1 className="hover:cursor-pointer">Mais Desejados</h1>
             <div className="submenu_maisDesejados">
