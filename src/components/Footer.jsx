606d72f2423eb5e6c11b4eeaf43bf11462959e5a
Jorgeluis.dev <122407806+jorgeluiisdev@users.noreply.github.com>
2024-07-20 00:54:59 -0300

    update-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,10 +13,10 @@ import { FaJava } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="">
-      <footer className="flex justify-center items-start gap-48 // w-full h-72  // mt-16 // bg-index-blue // text-sm text-white">
-        <div className="flex flex-col justify-start mt-10 ">
-          <h1 className="text-lg font-bold mb-4">Institucional</h1>
-          <ul className="flex flex-col gap-2">
+      <footer className="block justify-center items-center gap-48 // sm:flex sm:justify-between sm:items-center  // w-full h-full  // mt-16 // bg-index-blue // text-sm text-white">
+        <div className="flex flex-col justify-start mt-10 ml-5 //   sm:ml-9  ">
+          <h1 className="text-lg font-bold mb-4 mt-5">Institucional</h1>
+          <ul className="flex flex-col gap-1">
             <a href="#">
               <li>Quem Somos</li>
             </a>
@@ -37,7 +37,8 @@ const Footer = () => {
             </a>
           </ul>
         </div>
-        <div className="flex flex-col justify-start mt-10 ">
+         <hr className="hh mt-10"></hr> 
+        <div className="flex flex-col justify-start  mt-10 ml-5 // sm:ml-0">
           <h1 className="text-lg font-bold mb-4">Institucional</h1>
           <ul className="flex flex-col gap-2">
             <a href="#" className="flex items-center h-5">
@@ -54,34 +55,36 @@ const Footer = () => {
             <h1 className="font-bold">8h às 12h e 13h às 17:30</h1>
           </ul>
         </div>
-        <div className="flex flex-col justify-start mt-10 ">
+        <hr className="hh mt-10"></hr> 
+        <div className="flex flex-col justify-start mt-10 ml-5 // sm:ml-0">
           <h1 className="text-lg font-bold mb-2 ">Acompanhe</h1>
           <ul className="flex flex-col gap-2">
-            <div className="social_links flex gap-3 //">
-              <a href="#" className="w-1/3 text-3xl">
+            <div className="social_links flex">
+              <a href="#" className="w-10 text-3xl sm:w-1/3">
                 <FaYoutube />
               </a>
-              <a href="#" className="w-1/3 text-3xl">
+              <a href="#" className="w-10 text-3xl sm:w-1/3">
                 <FaInstagram />
               </a>
-              <a href="#" className="w-1/3 text-3xl">
+              <a href="#" className="w-10 text-3xl sm:w-1/3">
                 <FaSquareFacebook />
               </a>
             </div>
           </ul>
         </div>
-        <div className="flex flex-col justify-start mt-10 ">
-          <h1 className="text-lg font-bold mb-4 text-center">Compre Seguro</h1>
-          <ul className="flex flex-wrap justify-center items-center">
-            <FaCcVisa className="w-1/2 text-4xl mb-4" />
-            <SiAmericanexpress className="w-1/2 text-4xl mb-4" />
-            <FaCcMastercard className="w-1/2 text-4xl" />
-            <SiPagseguro className="w-1/2 text-4xl" />
+        <hr className="hh mt-10"></hr> 
+        <div className="flex flex-col justify-start mt-10 ml-5 // sm:ml-0">
+          <h1 className="text-lg font-bold mb-4 text-start // sm:text-start">Compre Seguro</h1>
+          <ul className="flex gap-5 // sm:flex-wrap sm:justify-center sm:items-center">
+            <FaCcVisa className="w-10 text-4xl mb-4" />
+            <SiAmericanexpress className="w-10 text-4xl mb-4" />
+            <FaCcMastercard className="w-10 text-4xl" />
+            <SiPagseguro className="w-10 text-4xl" />
           </ul>
         </div>
       </footer>
 
-      <div className="sub_footer // flex flex-wrap justify-center items-start gap-x-20 // w-full // bg-index-blue // text-sm text-white">
+      <div className="sub_footer // flex flex-wrap justify-start items-start //sm:justify-center sm:items-center// gap-x-20 // w-full // bg-index-blue // text-sm text-white // sm:justify-center ">
         <FaReact className="text-[70px]" />
         <FaJava className="text-[70px]" />
         <h1 className="w-full text-center mt-10 p-2 bg-cyan-600">
@@ -92,8 +95,19 @@ const Footer = () => {
         </h1>
       </div>
 
-      <div className="lll  flex justify-between items-center m-auto w-11/12">
-        <h5 className="text-footer flex justify-center items-center text-xs w-3/4 h-20 text-gray-400">
+      <div className="lll  flex flex-col items-start m-auto w-full mt-1 ml-2 sm:w-11/12">
+
+        <div className="certificados flex mt-2">
+          <a href="#" className="cert m-2">
+            <img src="/images/selo1.png" alt="Selo 1" className="w-14 sm:w-16" />
+          </a>
+          <a href="#" className="cert m-2">
+            <img src="/images/selo2.png" alt="Selo 2" className="w-14 sm:w-16" />
+          </a>
+        </div>
+        <hr className="hh mt-10"></hr> 
+        <h5 className="text-footer flex justify-center items-center text-xs min-w-80 h-14 mr-2 mb-5 //  text-gray-400 
+        sm:mb-5">
           Embora raro, os preços poderão variar durante a navegação sem aviso
           prévio. Pode ocorrer divergência entre o preço exibido no detalhe do
           produto e preço apresentado na sacola de compras, por questões
@@ -102,11 +116,6 @@ const Footer = () => {
           direitos reservados KYLY. CNPJ: 78.855.830/0001-98 Rodovia SC 418 KM
           3, Centro/SC CEP: 89107- 000.
         </h5>
-
-        <div className="certificados flex" >
-          <a href="#" className="cert m-2"><img src="/images/selo1.png" alt="Selo 1" className="w-16"/></a>
-          <a href="#" className="cert m-2"><img src="/images/selo2.png" alt="Selo 2" className="w-16"/></a>
-        </div>
         
       </div>
     </div>
